6f9783776f111bd7eaf4bbb30fd18831fba77432
NadavTechnion <nkaempfer@campus.technion.ac.il>
2026-01-19 11:58:58 +0200

    get_ith_collective_aura_squad implementaion

--- Huntech26a2.cpp
+++ Huntech26a2.cpp
@@ -151,48 +151,6 @@ output_t<int> Huntech::squad_duel(int squadId1, int squadId2) {
     }
 
 
-Squad& Huntech::find_winner_squad(int squadId1, int squadId2) {
-    int effective_aura_1, effective_aura_2;
-
-    Squad& squad1 = squadsTree.search(squadId1);
-    Squad& squad2 = squadsTree.search(squadId1);
-
-    int root_1 = squad1.getUnionHead();
-    int root_2 = squad2.getUnionHead();
-
-    int exp_1 = huntersUnion.exp(root_1);
-    int exp_2 = huntersUnion.exp(root_2);
-
-    int ab_1 = huntersUnion.ability(root_1).getEffectiveNenAbility();
-    int ab_2 = huntersUnion.ability(root_2).getEffectiveNenAbility();
-
-    // get aura
-    int Aura_1 = squad1.totalAura;
-    int Aura_2 = squad2.totalAura;
-
-    effective_aura_1 = exp_1 + Aura_1;
-    effective_aura_2 = exp_2 + Aura_2;
-    if(effective_aura_1 > effective_aura_2) {
-        Squad& winner = squad1;
-    }
-    else if(effective_aura_2 > effective_aura_1) {
-        Squad& winner= squad2;
-    }
-    else {
-        if(ab_1 > ab_2) {
-            Squad& winner = squad1;
-        }
-        else if(ab_2 > ab_1) {
-            Squad& winner = squad2;
-        }
-        else {
-            return output_t<int>(0); // draw
-        }
-    }
-
-    return 0;
-}
-
 output_t<int> Huntech::get_hunter_fights_number(int hunterId) {
     int fights = 0;
     if(hunterId <= 0) return output_t<int>(StatusType::INVALID_INPUT);
@@ -215,7 +173,7 @@ output_t<int> Huntech::get_squad_experience(int squadId) {
     if(squadId <= 0) return output_t<int>(StatusType::INVALID_INPUT);
     try {
         Squad& squad = squadsTree.search(squadId);
-        exp = huntersUnion.exp(squad.getUnionHead());
+        exp = huntersUnion.get_exp(squad.getUnionHead());
     }
     catch(bad_alloc& e) {
         return StatusType::ALLOCATION_ERROR;
@@ -227,7 +185,13 @@ output_t<int> Huntech::get_squad_experience(int squadId) {
 }
 
 output_t<int> Huntech::get_ith_collective_aura_squad(int i) {
-    return 0;
+    try {
+        Squad& target =  *squadsAuraTree.get_ith_element(i);
+        return target.totalAura;
+    }
+    catch (...){
+        return output_t<int>(StatusType::FAILURE);
+    }
 }
 
 output_t<NenAbility> Huntech::get_partial_nen_ability(int hunterId) {
